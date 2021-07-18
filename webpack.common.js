545d92d88e0033270984e94b0e47607526352c6d
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-07-17 23:29:47 -0300

    First commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].[contenthash].js',
+		clean: true
+	},
+	stats: {
+		colors: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	},
+	plugins: [new HtmlWebpackPlugin()]
+};
