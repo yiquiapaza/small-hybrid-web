45662a71c20e7eda8011cc73fa5b245e3ab22df3
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-08-03 13:57:01 -0300

    Update position and velocity when move elements

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ const hammer = new Hammer.Manager(square);
 const Swipe = new Hammer.Swipe();
 let [deltaX, deltaY] = [0, 0];
 
-let position = { position_x: 0, position_y: 0 };
+let position = { position_x: 0, position_y: 0, state: 0 };
 
 let [positionX, positionY] = [window.innerWidth, window.innerHeight];
 
-sendPostion(position);
+sendPostion(positionX, positionY);
 
 console.log(
 	square.getBoundingClientRect().top,
@@ -27,8 +27,9 @@ hammer.add(Swipe);
 hammer.on('swipe', (event) => {
 	deltaY = deltaY + event.deltaY;
 	deltaX = deltaX + event.deltaX;
-	position.position_x = +deltaX;
-	position.position_y = +deltaY;
+	position.position_x = (+deltaX * 0.2245) / positionX;
+	position.position_y = (-1 * +deltaY * 0.1395) / positionY;
+	position.state = 1;
 	sendPostion(position);
 	let translate3d = `translate3d(${deltaX}px, ${deltaY}px, 0)`;
 	event.target.style.transform = translate3d;
